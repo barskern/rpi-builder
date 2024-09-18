932cc06de52f3965d61016bd27c9904d33ece99e
Ole Martin Ruud <dev@barskern.no>
2024-09-18 15:12:48 +0200

    Add file to prevent usage of Xorg

--- configure-image.sh
+++ configure-image.sh
@@ -14,6 +14,24 @@ script="$2"
 [ -z "$mount" ] && { echo "Error: missing mount location as second argument"; exit 1; }
 [ -z "$script" ] && { echo "Error: missing script to run as second argument"; exit 1; }
 
+install_file() {
+	file="$1"
+	dest="$2"
+	perm="${3:-"644"}"
+	[ -z "$file" ] && { echo "Error: file argument empty"; exit 1; }
+	[ -z "$dest" ] && { echo "Error: dest argument empty"; exit 1; }
+
+	# Ensure dest is the path to the file itself, important for permission below
+	if [ "${dest: -1}" == "/" ]; then
+		dest="$dest$file"
+	fi
+
+	echo "Installing '$file' to '$dest' with '$perm'"
+
+	cp "src/$file" "$mount$dest"
+	chmod "$perm" "$mount$dest"
+}
+
 mount_module="rootfs-over-http"
 osrootfs="${RPI_OSROOTFS}"
 template="${RPI_HTML_TEMPLATE_NAME:-default}"
@@ -27,6 +45,10 @@ cat >$mount/boot/cmdline.txt <<EOF
 console=ttyAMA0,115200 console=tty1 logo.nologo ip=dhcp rd.info rd.noverifyssl osrootfs=$osrootfs
 EOF
 chmod 644 $mount/boot/cmdline.txt
+
+install_file 10-harden.conf /etc/X11/xorg.conf.d/
+install_file set-kiosk-url-from-cmdline.service /etc/systemd/system/
+install_file x11-autologin.service /etc/systemd/system/
 
 # Include some useful build info in the template if specified
 rpi_builder_tag="$(git describe --abbrev=0 2>/dev/null || true)"
@@ -45,21 +67,11 @@ cat src/$template_file | \
 	>$mount/home/rocky/default-page.html.template
 chmod 644 $mount/home/rocky/default-page.html.template
 
-cp src/set-kiosk-url-from-cmdline.sh $mount/home/rocky/
-chmod 755 $mount/home/rocky/set-kiosk-url-from-cmdline.sh
-
-cp src/xinitrc $mount/home/rocky/.xinitrc
-chmod 755 $mount/home/rocky/.xinitrc
-
-cp src/set-kiosk-url-from-cmdline.service $mount/etc/systemd/system/
-chmod 644 $mount/etc/systemd/system/set-kiosk-url-from-cmdline.service
-
-cp src/x11-autologin.service $mount/etc/systemd/system/
-chmod 644 $mount/etc/systemd/system/x11-autologin.service
-
-cp "${script}" "${mount}/tmp/${script}"
-chmod 755 "${mount}/tmp/${script}"
+install_file set-kiosk-url-from-cmdline.sh /home/rocky/ 755
+install_file xinitrc /home/rocky/.xinitrc 755
 
 # TODO Setup repos which should be used within the chroot
 
+cp "$script" "$mount/tmp/"
+chmod 755 "$mount/tmp/$script"
 chroot "$mount" "/tmp/$script"
